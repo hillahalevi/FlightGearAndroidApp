d5666c955b16d1c3934d6af9ca2a95e844c2bb9b
hilla halevi <hilla.halevi@live.biu.ac.il>
2019-06-20 17:34:51 +0300

    applied orientation, reset joystick borders,comments

--- app/src/main/java/com/example/flightgearandroidapp/JoystickActivity.java
+++ app/src/main/java/com/example/flightgearandroidapp/JoystickActivity.java
@@ -2,6 +2,7 @@ package com.example.flightgearandroidapp;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Point;
 import android.os.Bundle;
 import android.view.MotionEvent;
 import android.widget.Toast;
@@ -9,7 +10,37 @@ import android.widget.Toast;
 import com.example.flightgearandroidapp.services.ClientSide;
 import com.example.flightgearandroidapp.views.JoystickView;
 
-//TODO normalize the values to -1 --> 1
+class Info {
+    private float elevator;
+    private float aileron;
+    private double angle;
+    private double distance;
+
+    Info(float elevator, float aileron, double angle, double distance) {
+        this.elevator = elevator;
+        this.aileron = aileron;
+        this.angle = angle;
+        this.distance = distance;
+    }
+
+    public float getElevator() {
+        return elevator;
+    }
+
+    public float getAileron() {
+        return aileron;
+    }
+
+    public double getAngle() {
+        return angle;
+    }
+
+    public double getDistance() {
+        return distance;
+    }
+}
+
+
 public class JoystickActivity extends Activity {
     private ClientSide client;
     private JoystickView joystickView;
@@ -21,7 +52,13 @@ public class JoystickActivity extends Activity {
         this.joystickView = new JoystickView(this);
         setContentView(this.joystickView);
         this.isInJoystick = false;
+        connectToServer();
 
+    }
+
+
+    private void connectToServer() {
+        //TODO with jenny
         Intent intent = getIntent();
         String ip = intent.getStringExtra("ip");
         int port = intent.getIntExtra("port", 5400);
@@ -30,51 +67,36 @@ public class JoystickActivity extends Activity {
         this.client.connect(ip, port);
     }
 
+    /**
+     * Is responsible for the joystick-moving logic-whenever a touch accrues
+     *
+     * @param event - the gesture the user acted on the screen
+     * @return true if a relevant action has performed, false otherwise
+     */
     @Override
     public boolean onTouchEvent(MotionEvent event) {
         int action = event.getActionMasked();
-        int touchX = (int) event.getRawX();
-        int touchY = (int) event.getRawY();
-
-        Toast.makeText(JoystickActivity.this, "(x:" + touchX + ", y:" + touchY + ")", Toast.LENGTH_SHORT).show();
-
+        Point touchPoint = new Point();
+        touchPoint.x = (int) event.getRawX();
+        touchPoint.y = (int) event.getRawY();
         switch (action) {
             case MotionEvent.ACTION_DOWN: {
-                // if the touch happened outside the joystick then ignore it
-                if (!this.isInsideJoystick(touchX, touchY)) {
-                    return false;
+                if (!this.isInsideJoystick(touchPoint)) {
+                    return false; //irrelevant touch
                 }
-                // otherwise, update the flag for upcoming move actions
-                this.isInJoystick = true;
+                this.isInJoystick = true; //update the flag for upcoming  actions
                 break;
             }
             case MotionEvent.ACTION_MOVE: {
                 if (!this.isInJoystick) {
-                    return false;
+                    return false; //irrelevant touch
                 }
-                // get the values to send to the client
-                double distance = this.distance(touchX, touchY, this.joystickView.getCenterX(), this.joystickView.getCenterY());
-                double magnitude = distance / this.joystickView.getOuterRadius();
-                if (magnitude >= 1) {
-                    magnitude = 1;
-                }
-                double angle = this.getAngle(touchX - this.joystickView.getCenterX(), touchY - this.joystickView.getCenterY());
-//                this.textView.setText(String.valueOf((int)angle));
-
-                double elevator = Math.sin(Math.toRadians(angle)) * magnitude * -1;
-                double aileron = Math.cos(Math.toRadians(angle)) * magnitude;
-
-                this.client.sendCommand("elevator", String.valueOf(elevator));
-                this.client.sendCommand("aileron", String.valueOf(aileron));
-
-                // draw the new position
-                int[] newPos = this.getAdjustedPosition(touchX, touchY, angle, distance);
-                this.updateJoystickPosition(newPos[0], newPos[1]);
+                move(touchPoint);
                 break;
             }
-            case MotionEvent.ACTION_UP:         // fallthrough
+            case MotionEvent.ACTION_UP:
             case MotionEvent.ACTION_CANCEL: {
-                // place the joystick in its original position
+                // reCenter the joystick
                 this.updateJoystickPosition(this.joystickView.getCenterX(), this.joystickView.getCenterY());
                 this.isInJoystick = false;
                 break;
@@ -83,20 +105,77 @@ public class JoystickActivity extends Activity {
         return true;
     }
 
-    private boolean isInsideJoystick(int touchX, int touchY) {
-        return this.distance(touchX, touchY, this.joystickView.getCurrX(), this.joystickView.getCurrY()) <=
+    /**
+     * normalize the values of the movement
+     * @return
+     */
+    private Info getNormalizeValues(Point touchPoint) {
+        int outerRadius = this.joystickView.getOuterRadius();
+        int innerRadius = this.joystickView.getInnerRadius();
+        int centerX = this.joystickView.getCenterX();
+        int centerY = this.joystickView.getCenterY();
+        double distance = this.distance(touchPoint.x, touchPoint.y, centerX, centerY);
+        double magnitude = (distance + innerRadius) / outerRadius;
+        if (magnitude > 1) {
+            magnitude = 1;
+        }
+        double angle = this.getAngle(touchPoint.x - centerX, touchPoint.y - centerY);
+        float elevator = (float) (Math.sin(Math.toRadians(angle)) * magnitude * -1);
+        float aileron = (float) (Math.cos(Math.toRadians(angle)) * magnitude);
+        return new Info(elevator, aileron, angle, distance);
+    }
+
+
+    private void move(Point touchPoint) {
+        Info info = getNormalizeValues(touchPoint);
+        //see the values
+        Toast.makeText(JoystickActivity.this, "(elevator:" + info.getElevator() + ", aileron:" + info.getAileron() + ")", Toast.LENGTH_SHORT).show();
+        //todo check with jenny
+        this.client.sendCommand("elevator", String.valueOf(info.getElevator()));
+        this.client.sendCommand("aileron", String.valueOf(info.getAileron()));
+
+        // draw the new position
+        Point newPos = this.AdjustPosition(touchPoint.x, touchPoint.y, info.getAngle(), info.getDistance());
+        this.updateJoystickPosition(newPos.x, newPos.y);
+
+    }
+
+
+    /**
+     * check if the touch is inside the joystick range
+     *
+     * @return
+     */
+    private boolean isInsideJoystick(Point touchPoint) {
+        return this.distance(touchPoint.x, touchPoint.y, this.joystickView.getCurrX(), this.joystickView.getCurrY()) <=
                 this.joystickView.getInnerRadius();
     }
 
+    /**
+     * calculate the distance between two points
+     *
+     * @param x1
+     * @param y1
+     * @param x2
+     * @param y2
+     * @return
+     */
     private double distance(float x1, float y1, float x2, float y2) {
         return Math.sqrt(Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2));
     }
 
+    /**
+     * calculate the angle between two points
+     *
+     * @param dx
+     * @param dy
+     * @return
+     */
     private double getAngle(float dx, float dy) {
         double angle = 0;
         if (dx >= 0 && dy >= 0) {
             angle = Math.toDegrees(Math.atan(dy / dx));
-        } else if ((dx < 0 && dy >= 0) ||(dx < 0 && dy < 0)) {
+        } else if ((dx < 0 && dy >= 0) || (dx < 0 && dy < 0)) {
             angle = Math.toDegrees(Math.atan(dy / dx)) + 180;
         } else if (dx >= 0 && dy < 0) {
             angle = Math.toDegrees(Math.atan(dy / dx)) + 360;
@@ -104,18 +183,36 @@ public class JoystickActivity extends Activity {
         return angle;
     }
 
-    private int[] getAdjustedPosition(int touchX, int touchY, double angle, double distanceFromCenter) {
+    /**
+     * correcting the touch position if occurred outside the joystick
+     *
+     * @param touchX
+     * @param touchY
+     * @param angle
+     * @param distanceFromCenter
+     * @return
+     */
+    private Point AdjustPosition(int touchX, int touchY, double angle, double distanceFromCenter) {
         int outerRadius = this.joystickView.getOuterRadius();
-        if (distanceFromCenter <= outerRadius) {
+        int innerRadius = this.joystickView.getInnerRadius();
+        if (distanceFromCenter + innerRadius <= outerRadius) {
             //irrelevant touch
-            return new int[]{touchX, touchY};
+            return new Point(touchX, touchY);
         }
+
+        int distance = outerRadius - innerRadius;
         // placing the joystick  relatively
-        int newX = this.joystickView.getCenterX() + (int) (Math.cos(Math.toRadians(angle)) * outerRadius);
-        int newY = this.joystickView.getCenterY() + (int) (Math.sin(Math.toRadians(angle)) * outerRadius);
-        return new int[]{newX, newY};
+        int newX = this.joystickView.getCenterX() + (int) (Math.cos(Math.toRadians(angle)) * distance);
+        int newY = this.joystickView.getCenterY() + (int) (Math.sin(Math.toRadians(angle)) * distance);
+        return new Point(newX, newY);
     }
 
+    /**
+     * set the new position of the joystick
+     *
+     * @param newX
+     * @param newY
+     */
     private void updateJoystickPosition(int newX, int newY) {
         this.joystickView.setX(newX);
         this.joystickView.setY(newY);
--- app/src/main/java/com/example/flightgearandroidapp/views/JoystickView.java
+++ app/src/main/java/com/example/flightgearandroidapp/views/JoystickView.java
@@ -1,61 +1,40 @@
 package com.example.flightgearandroidapp.views;
 
-import android.app.Activity;
 import android.content.Context;
-import android.content.res.Configuration;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Point;
 import android.util.DisplayMetrics;
 import android.view.View;
 
+/**
+ * The class is responsible for the joystick display
+ */
 public class JoystickView extends View {
-    private int OUTER_RADIUS;
-    private int INNER_RADIUS;
-    private int CENTER_X;
-    private int CENTER_Y;
-
-    private int currX;
-    private int currY;
-
     private Paint outerCircle;
     private Paint innerCircle;
     private Paint backgroundColor;
+    private int CENTER_X;
+    private int CENTER_Y;
+    private int currX;
+    private int currY;
+    private int OUTER_RADIUS;
+    private int INNER_RADIUS;
+
+    private int androidStatusBarHeight;
 
     public JoystickView(Context context) {
         super(context);
-        setJoystick();
-        DisplayMetrics metrics = new DisplayMetrics();
-        ((Activity) context).getWindowManager().getDefaultDisplay().getMetrics(metrics);
-        currX = CENTER_X = metrics.widthPixels / 2;
-        currY = CENTER_Y = metrics.heightPixels / 2;
-        OUTER_RADIUS = metrics.widthPixels / 3;
-        INNER_RADIUS = OUTER_RADIUS / 3;
+        setJoystickColors();
+        setJoystickParams();
     }
 
-    @Override
-    protected void onDraw(Canvas canvas) {
-        super.onDraw(canvas);
-        canvas.drawRect(0, 0, CENTER_X * 2, CENTER_Y * 2, backgroundColor);
-        canvas.drawCircle(CENTER_X, CENTER_Y, OUTER_RADIUS, outerCircle);
-        canvas.drawCircle(currX, currY, INNER_RADIUS, innerCircle);
-    }
 
-    @Override
-    protected void onSizeChanged(int w, int h, int oldWeight, int oldHeight) {
-        currX = CENTER_X = w / 2;
-        currY = CENTER_Y = h / 2;
-        int orientation = getResources().getConfiguration().orientation;
-        if (orientation == Configuration.ORIENTATION_PORTRAIT) {
-            OUTER_RADIUS = CENTER_X * 2 / 3;
-        } else if (orientation == Configuration.ORIENTATION_LANDSCAPE) {
-            OUTER_RADIUS = CENTER_Y * 2 / 3;
-        }
-        INNER_RADIUS = OUTER_RADIUS / 3;
-        super.onSizeChanged(w, h, oldWeight, oldHeight);
-    }
-
-    private void setJoystick() {
+    /**
+     * set joystick colors
+     */
+    private void setJoystickColors() {
         this.outerCircle = new Paint(Paint.ANTI_ALIAS_FLAG);
         this.outerCircle.setColor(Color.WHITE);
         this.outerCircle.setStyle(Paint.Style.FILL);
@@ -69,20 +48,61 @@ public class JoystickView extends View {
         this.backgroundColor.setStyle(Paint.Style.FILL);
     }
 
-    public int getOuterRadius() {
-        return this.OUTER_RADIUS;
+    /**
+     * set it nicely in the screen
+     */
+    private void setJoystickParams() {
+        // get the dimensions of the actual screen
+        DisplayMetrics dm = getResources().getDisplayMetrics();
+        currX = CENTER_X = dm.widthPixels / 2;
+        currY = CENTER_Y = dm.heightPixels / 2;
+        OUTER_RADIUS = dm.widthPixels / 3;
+        INNER_RADIUS = OUTER_RADIUS / 3;
+        this.androidStatusBarHeight = this.getAndroidStatusBarHeight() * (int) dm.density;
+
     }
 
-    public int getInnerRadius() {
-        return this.INNER_RADIUS;
+
+    /**
+     * Get the height of the android status bar
+     *
+     * @return the height of the status bar
+     */
+    private int getAndroidStatusBarHeight() {
+        int resourceId = getResources().getIdentifier("status_bar_height", "dimen", "android");
+        if (resourceId > 0) {
+            return getResources().getDimensionPixelSize(resourceId);
+        }
+        return 0;
     }
 
-    public int getCenterX() {
-        return CENTER_X;
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        canvas.drawRect(0, 0, CENTER_X * 2, CENTER_Y * 2, backgroundColor);
+        canvas.drawCircle(CENTER_X, CENTER_Y - androidStatusBarHeight, OUTER_RADIUS, outerCircle);
+        canvas.drawCircle(currX, currY - androidStatusBarHeight, INNER_RADIUS, innerCircle);
     }
 
-    public int getCenterY() {
-        return CENTER_Y;
+
+    /**
+     * A change in orientation has bees made -replace the joystick
+     *
+     * @param w    - the width of the current orientation
+     * @param h    - the height of the current orientation
+     * @param oldw - the width of the previous orientation
+     * @param oldh - the height of the previous orientation
+     */
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        currX = CENTER_X = w / 2;
+        currY = CENTER_Y = (h + androidStatusBarHeight) / 2;
+
+        OUTER_RADIUS = Math.min(CENTER_X, CENTER_Y) * 2 / 3;
+        INNER_RADIUS = OUTER_RADIUS / 3;
+
+        super.onSizeChanged(w, h, oldw, oldh);
     }
 
     public int getCurrX() {
@@ -93,11 +113,29 @@ public class JoystickView extends View {
         return this.currY;
     }
 
+
     public void setX(int x) {
         this.currX = x;
     }
 
     public void setY(int y) {
         this.currY = y;
+    }
+
+
+    public int getCenterX() {
+        return CENTER_X;
+    }
+
+    public int getCenterY() {
+        return CENTER_Y;
+    }
+
+    public int getOuterRadius() {
+        return OUTER_RADIUS;
+    }
+
+    public int getInnerRadius() {
+        return INNER_RADIUS;
     }
 }
